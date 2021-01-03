01479f122e724ae61309e0cf7e7ee2356b44f01b
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2021-01-03 09:44:50 -0700

    add Nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import React from 'react'
 import GlobalStyle from './components/GlobalStyle'
 //Screen imports
 import AboutScreen from './screens/AboutScreen'
+//Component imports
+import Nav from './components/Nav'
 
 function App() {
 	return (
 		<div className='App'>
 			<GlobalStyle />
+			<Nav />
 			<AboutUs />
 		</div>
 	)
