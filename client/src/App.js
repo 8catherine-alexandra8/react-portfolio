c1a2c960f2ac25a79af937228d5e3b010583793d
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2021-01-02 17:09:11 -0700

    add styled components to Faq section and About section. Created GlobalStyles and styles.js for styling About Screen as a whole

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+//Global Style
+import GlobalStyle from './components/GlobalStyle'
 //Screen imports
 import AboutScreen from './screens/AboutScreen'
 
 function App() {
 	return (
 		<div className='App'>
+			<GlobalStyle />
 			<AboutUs />
 		</div>
 	)
